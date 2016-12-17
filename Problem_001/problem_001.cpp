3a6ab44ab970574c31a4e9019726bd088a7674c9
Jack Draak <jackdraak@gmail.com>
2016-12-17 05:46:47 -0500

    stab one says wrong answer

--- Problem_001/problem_001.cpp
+++ Problem_001/problem_001.cpp
@@ -5,5 +5,58 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
+#include <vector>
+#include <sstream>
+#include <iostream>
 
-int main() { return 0; }
+void AddToSet(int);
+int TallySet();
+
+int const M1 = 3; // core multiplier
+int const M2 = 5; // core multiplier
+int const TOP = 1000; // exclusive
+
+std::vector<int> thisSet;
+
+int main() 
+{ 
+    thisSet.swap(std::vector<int>());
+    AddToSet(M1);
+    AddToSet(M2);
+    std::cout << "\n\nFINAL ANALYSIS: " << TallySet();
+    // Wait for input before exiting main.
+    std::string userInput;
+    std::cout << "\nhit <Enter> to exit...\n";
+    getline(std::cin, userInput);
+    return 0;
+}
+
+int TallySet()
+{
+    int tally = 0;
+    int setSize = thisSet.size();
+    if (setSize > 0)
+    {
+        for (int i = 0; i < setSize; i++)
+        {
+            tally += thisSet[i];
+        }
+    }
+    return tally;
+}
+
+void AddToSet(int thatMultiplier)
+{
+    bool bComplete = false;
+    int thisMultiplier = 0;
+    int thisProduct = 0;
+    std::cout << "\nBuilding set for x" << thatMultiplier << "...";
+    do
+    {
+        thisMultiplier++;
+        std::cout << thisMultiplier << ".";
+        thisProduct = thisMultiplier * thatMultiplier;
+        if (thisProduct < TOP) thisSet.push_back(thisProduct);
+        else bComplete = true;
+    } while (!bComplete);
+}
